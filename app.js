6bca5b2262aa3e925350f6782236f23d576e00ac
sevim <sevim.gokturk@powercoders.org>
2022-11-18 09:08:40 +0100

    fri 9.08

--- app.js
+++ app.js
@@ -6,33 +6,55 @@ let key ="6c8df01f"
 
     let value="";
     let pagenr=1;
+    //asigne the input vatue
     input.addEventListener("input", (e)=>{
     e.preventDefault();
     value=input.value; 
     });
+
+    //we add event listener to make active the getmovie function
+    input.addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("button").click();
+        }
+    });
+
+    //ane also we can make active the getmovie function withe button
     button.addEventListener("click",()=>{    
         pagenr=1;
         getmovie(value,pagenr);
         document.getElementById("body").style.backgroundImage = "none";
     });
+
+    //we use asencronic function  because it is necesserie to wait fetch data. 
+    //if the connection is slow it is waiting the data
     async function getmovie(value,p){
         if(value==="") return;
+
+        //await is here 
+        //we use omdb API and we took api keys for connection
         const data =await fetch(
         'http://www.omdbapi.com/?apikey='+key+'&s='+value+'&page='+pagenr  
         );
+
         //first clean the inner html
         document.querySelector(".display").innerHTML ="";
-        // convert the result to json
+
+        // convert the result to json also we add await
         const result = await data.json();
         console.log(result);
+
         //in the response there is a Search data, under the search we can acces the info like year, imdb post, title
+        //with  this loop we create the posters and the descriptions for each item of the result 
         result.Search. forEach( (item) => {
             let moviediv = document.createElement ("div") ;
             moviediv.classList.add("movie");
             let poster = document.createElement("div");
             poster.classList.add ("poster") ;
             let img = document.createElement("img") ;
-        // if inside the fetch data ther is no Ppster (image, not available:N/A) use this photo
+
+        //inside the fetch data if ther is no Ppster (image, not available:N/A) use this photo
             img.src=
             item. Poster === "N/A" ? (img.src = "./pictures/42862.jpg") :item.Poster;
             poster.appendChild(img);
@@ -42,20 +64,23 @@ let key ="6c8df01f"
             description.innerHTML= "Title :"+item.Title +"<br><br>"+"Year :" +item.Year+"<br><br>" + "<a href=https://www.imdb.com/title/"+item.imdbID+">IMDB:"+ "https://www.imdb.com/title/"+item.imdbID+"</a> "
             moviediv.appendChild(description);
             document.querySelector(".display").appendChild(moviediv);});
+            //we are making visible the net and previus buttons
             next.classList.add("visible");
             prev.classList.add("visible");
     }
+
     //to pass the next page
-    //some notes???
     next.addEventListener("click",()=>{
-        if(value==="")return;
-        pagenr++;
+        if(value==="")return;  // if ther is no value finish the function
+        pagenr++; //we are incrising the page number and calling the getmovie wihe new parematers
         getmovie(value,pagenr)
     });
+
+    //to pass the previus page
     prev.addEventListener("click",()=>{
-        if(value==="")return;
-        if(pagenr===1)return;
-        pagenr--;
+        if(value==="")return; // if ther is no value finish the function
+        if(pagenr===1)return; // if the page number is 1, also finish the function
+        pagenr--; //we are ddecreasing the page number and calling the getmovie with new parematers
         getmovie(value,pagenr)
     });
 
